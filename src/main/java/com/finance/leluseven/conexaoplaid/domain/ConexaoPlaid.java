2506ba1fb5cd3324de58f950f7cb8827d0d07e69
LUCAS DE OLIVEIRA CARVALHO <lucas.ocarvalho@cbm.df.gov.br>
2026-07-02 12:01:35 -0300

    Fix: Inserindo classe agregado raiz para conexao ao plaid!

---
+++ src/main/java/com/finance/leluseven/conexaoplaid/domain/ConexaoPlaid.java
@@ -0,0 +1,51 @@
+package com.finance.leluseven.conexaoplaid.domain;
+
+import com.finance.leluseven.conexaoplaid.domain.vo.AccessTokenPlaid;
+import com.finance.leluseven.conexaoplaid.domain.vo.CodConexaoPlaid;
+import com.finance.leluseven.conexaoplaid.domain.vo.CursorPlaid;
+import com.finance.leluseven.conexaoplaid.domain.vo.ItemIdPlaid;
+import com.finance.leluseven.shared.exception.DomainException;
+import com.finance.leluseven.usuario.domain.vo.CodUsuario;
+import lombok.Getter;
+
+@Getter
+public class ConexaoPlaid {
+    private CodConexaoPlaid codConexaoPlaid;
+    private CodUsuario codUsuario;
+    private AccessTokenPlaid accessToken;  // autentica chamadas
+    private ItemIdPlaid itemId;       // identifica a conexão
+    private CursorPlaid plaidCursor;       // progresso da sincronização
+    private String instituicao;  // "Nubank", "Itaú"...
+    private boolean ativo;
+
+    public static ConexaoPlaid criar(String accessToken, String itemId, String instituicao) {
+        var conexao = new ConexaoPlaid();
+        conexao.accessToken = AccessTokenPlaid.de(accessToken);
+        conexao.itemId      = ItemIdPlaid.de(itemId);
+        conexao.instituicao = instituicao;
+        return conexao;
+    }
+
+    public static ConexaoPlaid vincularUsuario(Long codUsuario, String accessToken, String itemId, String instituicao) {
+        var conexao = new ConexaoPlaid();
+        conexao.codUsuario = CodUsuario.de(codUsuario);
+        conexao.accessToken = AccessTokenPlaid.de(accessToken);
+        conexao.itemId      = ItemIdPlaid.de(itemId);
+        conexao.instituicao = instituicao;
+        return conexao;
+    }
+
+    public static ConexaoPlaid reconstituir(Long codConexao, Long codUsuario, String accessToken, String itemId, String instituicao) {
+        var conexao = new ConexaoPlaid();
+        conexao.codConexaoPlaid = CodConexaoPlaid.de(codConexao);
+        conexao.codUsuario = CodUsuario.de(codUsuario);
+        conexao.accessToken = AccessTokenPlaid.de(accessToken);
+        conexao.itemId      = ItemIdPlaid.de(itemId);
+        conexao.instituicao = instituicao;
+        return conexao;
+    }
+
+    public void atualizarPlaidCursor(String cursor) {
+        this.plaidCursor = CursorPlaid.de(cursor);
+    }
+}
